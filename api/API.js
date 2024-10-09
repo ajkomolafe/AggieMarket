31e8fa4fbbe69df93377a77b77e6d4d6551bbded
ajkomolafe <141524434+ajkomolafe@users.noreply.github.com>
2024-10-09 13:57:43 -0500

    Completed user router for api

--- api/API.js
+++ api/API.js
@@ -1,17 +1,18 @@
-require("dotenv").config(); //Doesn't need to be called after config
+require("dotenv").config();
 const mongoClient = require('mongoose');
 const express = require("express");
 const mongoose = require("mongoose");
 const api = express();
 
+//MongoDB connection setup
 mongoose.connect(process.env.DATABASE_URI);
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Database"));
 
-api.use(express.json()); //Allows api to take json requests
-
-const userRouter = require("./routers/users"); //aggiemarketdata > users
+//Express.js router and listening setup
+api.use(express.json());
+const userRouter = require("./users/users.js");
 api.use("/users", userRouter) //requests on localhost:3000/users go to userRouter
 
 api.listen(3000, () => console.log("Server Started"));
--- api/schemas/user.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-//Input validation schema
-const userSchema = mongoose.Schema({
-
-})
